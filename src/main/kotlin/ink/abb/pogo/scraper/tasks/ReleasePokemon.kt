d7fcf8eb98ca88a4fccb0b11d8b3c90081372b7c
Jasper Abbink <git@abb.ink>
2016-07-23 17:15:32 +0200

    Don't transfer nicknamed or favourited Pokemon Fixes #84

--- src/main/kotlin/ink/abb/pogo/scraper/tasks/ReleasePokemon.kt
+++ src/main/kotlin/ink/abb/pogo/scraper/tasks/ReleasePokemon.kt
@@ -29,33 +29,38 @@ class ReleasePokemon : Task {
         groupedPokemon.forEach {
             val sorted = it.value.sortedByDescending { it.cp }
             for ((index, pokemon) in sorted.withIndex()) {
-                val iv = pokemon.getIv()
-                val ivPercentage = pokemon.getIvPercentage()
-                // never transfer highest rated Pokemon
-                if (index > 0) {
-                    // stop releasing when pokemon is set in ignoredPokemon
-                    if (!ignoredPokemon.contains(pokemon.pokemonId.name)) {
-                        var shouldRelease = obligatoryTransfer.contains(pokemon.pokemonId.name)
-                        var reason = ""
-                        if (shouldRelease) {
-                            reason = "Obligatory release"
-                        } else {
-                            // never transfer > maxIv, unless set in obligatoryTransfer
-                            if (ivPercentage < minIVPercentage) {
-                                reason = "IV < minimum IV percentage"
-                                shouldRelease = true
+                // don't drop favourited or nicknamed pokemon
+                val isFavourite = pokemon.nickname.isNotBlank() || pokemon.favorite
+                if (!isFavourite) {
+                    val iv = pokemon.getIv()
+                    val ivPercentage = pokemon.getIvPercentage()
+                    // never transfer highest rated Pokemon
+                    if (index > 0) {
+                        // stop releasing when pokemon is set in ignoredPokemon
+                        if (!ignoredPokemon.contains(pokemon.pokemonId.name)) {
+                            var shouldRelease = obligatoryTransfer.contains(pokemon.pokemonId.name)
+                            var reason = ""
+                            if (shouldRelease) {
+                                reason = "Obligatory release"
+                            } else {
+                                // never transfer > maxIv, unless set in obligatoryTransfer
+                                if (ivPercentage < minIVPercentage) {
+                                    reason = "IV < minimum IV percentage"
+                                    shouldRelease = true
+                                }
+                                // never transfer > maxCP, unless set in obligatoryTransfer
+                                if (pokemon.cp < minCP) {
+                                    reason = "CP < minimum CP"
+                                    // only set it to true, when it already was true, otherwise don't release
+                                    shouldRelease = shouldRelease && true
+                                }
                             }
-                            // never transfer > maxCP, unless set in obligatoryTransfer
-                            if (pokemon.cp < minCP) {
-                                reason = "CP < minimum CP"
-                                // only set it to true, when it already was true, otherwise don't release
-                                shouldRelease = shouldRelease && true
+                            if (shouldRelease) {
+                                ctx.pokemonStats.second.andIncrement
+                                Log.yellow("Going to transfer ${pokemon.pokemonId.name} with " +
+                                        "CP ${pokemon.cp} and IV $iv%; reason: $reason")
+                                pokemon.transferPokemon()
                             }
-                        }
-                        if (shouldRelease) {
-                            ctx.pokemonStats.second.andIncrement
-                            Log.yellow("Going to transfer ${pokemon.pokemonId.name} with CP ${pokemon.cp} and IV $iv%; reason: $reason")
-                            pokemon.transferPokemon()
                         }
                     }
                 }
