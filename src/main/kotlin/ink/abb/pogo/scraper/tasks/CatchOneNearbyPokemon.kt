dfd9e974a9f6a0685ddc018c5698faca589e45f3
Jasper Abbink <git@abb.ink>
2016-07-24 10:14:17 +0200

    allow keeping more than 1 pokemon per type

--- src/main/kotlin/ink/abb/pogo/scraper/tasks/CatchOneNearbyPokemon.kt
+++ src/main/kotlin/ink/abb/pogo/scraper/tasks/CatchOneNearbyPokemon.kt
@@ -8,13 +8,14 @@
 
 package ink.abb.pogo.scraper.tasks
 
-import ink.abb.pogo.scraper.util.Log
 import POGOProtos.Inventory.Item.ItemIdOuterClass.ItemId
 import POGOProtos.Networking.Responses.CatchPokemonResponseOuterClass.CatchPokemonResponse
 import ink.abb.pogo.scraper.Bot
 import ink.abb.pogo.scraper.Context
 import ink.abb.pogo.scraper.Settings
 import ink.abb.pogo.scraper.Task
+import ink.abb.pogo.scraper.util.Log
+import ink.abb.pogo.scraper.util.pokemon.*
 
 class CatchOneNearbyPokemon : Task {
     override fun run(bot: Bot, ctx: Context, settings: Settings) {
@@ -48,22 +49,28 @@ class CatchOneNearbyPokemon : Task {
                 ctx.api.setLocation(ctx.lat.get(), ctx.lng.get(), 0.0)
                 val encounterResult = catchablePokemon.encounterPokemon()
                 if (encounterResult.wasSuccessful()) {
-                    Log.green("Encountered pokemon ${catchablePokemon.pokemonId} with CP ${encounterResult.wildPokemon.pokemonData.cp}")
-                    val result = catchablePokemon.catchPokemon(usedPokeball)
-
+                    Log.green("Encountered pokemon ${catchablePokemon.pokemonId} " +
+                            "with CP ${encounterResult.wildPokemon.pokemonData.cp}")
+                    val result = catchablePokemon.catch(
+                            normalizedHitPosition = 1.0,
+                            normalizedReticleSize = 1.95 + Math.random() * 0.05,
+                            spinModifier = 0.85 + Math.random() * 0.15,
+                            ballType = usedPokeball)!!
                     if (result.status == CatchPokemonResponse.CatchStatus.CATCH_SUCCESS) {
                         ctx.pokemonStats.first.andIncrement
-                        var message = "Caught a ${catchablePokemon.pokemonId} with CP ${encounterResult.wildPokemon.pokemonData.cp} using $ball"
+                        var message = "Caught a ${catchablePokemon.pokemonId} " +
+                                "with CP ${encounterResult.wildPokemon.pokemonData.cp} using $ball"
 
                         if (settings.shouldDisplayPokemonCatchRewards)
-                            message += ": [${result.xpList.sum()}x XP, ${result.candyList.sum()}x Candy, ${result.stardustList.sum()}x Stardust]"
+                            message += ": [${result.xpList.sum()}x XP, ${result.candyList.sum()}x " +
+                                    "Candy, ${result.stardustList.sum()}x Stardust]"
                         Log.green(message)
 
                     } else
                         Log.red("Capture of ${catchablePokemon.pokemonId} failed with status : ${result.status}")
                 } else {
                     Log.red("Encounter failed with result: ${encounterResult.getStatus()}")
-                } 
+                }
             }
 
         }
