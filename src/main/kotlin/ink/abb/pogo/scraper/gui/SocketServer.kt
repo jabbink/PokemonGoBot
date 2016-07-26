c9a7c313aee6d73731840a7d9d040b6aed848091
shilch <shilch@github.com>
2016-07-26 23:42:02 +0200

    Moved requiredXp to new file

--- src/main/kotlin/ink/abb/pogo/scraper/gui/SocketServer.kt
+++ src/main/kotlin/ink/abb/pogo/scraper/gui/SocketServer.kt
@@ -8,13 +8,10 @@ import com.pokegoapi.api.player.PlayerProfile
 import com.pokegoapi.api.pokemon.Pokemon
 import com.pokegoapi.google.common.geometry.S2LatLng
 import ink.abb.pogo.scraper.Context
+import ink.abb.pogo.scraper.requiredXp
 import ink.abb.pogo.scraper.util.pokemon.getIvPercentage
 
 class SocketServer {
-    val requiredXp = arrayOf(0, 1000, 3000, 6000, 10000, 15000, 21000, 28000, 36000, 45000, 55000, 65000, 75000,
-            85000, 100000, 120000, 140000, 160000, 185000, 210000, 260000, 335000, 435000, 560000, 710000, 900000, 1100000,
-            1350000, 1650000, 2000000, 2500000, 3000000, 3750000, 4750000, 6000000, 7500000, 9500000, 12000000, 15000000, 20000000)
-
     private var ctx: Context? = null
     private var server: SocketIOServer? = null
 
--- src/main/kotlin/ink/abb/pogo/scraper/tasks/UpdateProfile.kt
+++ src/main/kotlin/ink/abb/pogo/scraper/tasks/UpdateProfile.kt
@@ -9,19 +9,11 @@
 package ink.abb.pogo.scraper.tasks
 
 import com.pokegoapi.api.player.PlayerProfile
-import ink.abb.pogo.scraper.Bot
-import ink.abb.pogo.scraper.Context
-import ink.abb.pogo.scraper.Settings
-import ink.abb.pogo.scraper.Task
+import ink.abb.pogo.scraper.*
 import ink.abb.pogo.scraper.util.Log
 import java.text.DecimalFormat
 
 class UpdateProfile : Task {
-
-    val requiredXp = arrayOf(0, 1000, 3000, 6000, 10000, 15000, 21000, 28000, 36000, 45000, 55000, 65000, 75000,
-            85000, 100000, 120000, 140000, 160000, 185000, 210000, 260000, 335000, 435000, 560000, 710000, 900000, 1100000,
-            1350000, 1650000, 2000000, 2500000, 3000000, 3750000, 4750000, 6000000, 7500000, 9500000, 12000000, 15000000, 20000000)
-
     override fun run(bot: Bot, ctx: Context, settings: Settings) {
         val player = ctx.api.playerProfile
         try {
