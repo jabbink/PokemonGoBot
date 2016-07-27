95eecdf47660eb39c600e8d0d9385ed94b012b8c
CosmoRing <cosmoringhub@gmail.com>
2016-07-27 01:25:33 +0100

    Fixing bot not walking, only getting map

--- src/main/kotlin/ink/abb/pogo/scraper/tasks/WalkToUnusedPokestop.kt
+++ src/main/kotlin/ink/abb/pogo/scraper/tasks/WalkToUnusedPokestop.kt
@@ -22,7 +22,7 @@ class WalkToUnusedPokestop(val sortedPokestops: List<Pokestop>, val lootTimeouts
     override fun run(bot: Bot, ctx: Context, settings: Settings) {
         // don't run away when there are still Pokemon around
         val pokemonCount = ctx.api.map?.catchablePokemon?.filter { !ctx.blacklistedEncounters.contains(it.encounterId) }?.size
-        if (pokemonCount != null && pokemonCount > 0) {
+        if (pokemonCount != null && pokemonCount > 0 && settings.shouldCatchPokemons) {
             return
         }
         if (!ctx.walking.compareAndSet(false, true)) {
