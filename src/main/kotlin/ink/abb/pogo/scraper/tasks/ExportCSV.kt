48d0b78871ce691a9df8a93d267c0ec474864ac7
Kevin <Kevin.Gaytant@ArgusTech.be>
2016-07-28 13:39:09 +0200

    Catch any thrown exceptions while trying to write the file (fixes ugly exception when file is open by MS Excel)

--- src/main/kotlin/ink/abb/pogo/scraper/tasks/ExportCSV.kt
+++ src/main/kotlin/ink/abb/pogo/scraper/tasks/ExportCSV.kt
@@ -37,36 +37,40 @@ class ExportCSV : Task {
             }
         }
 
-        // UTF-8 with BOM to fix borked UTF-8 chars in MS Excel (for nickname output)
-        // https://en.wikipedia.org/wiki/Byte_order_mark#UTF-8
-        val os = FileOutputStream("export.csv")
-        os.write(239);
-        os.write(187);
-        os.write(191);
-        val writer = PrintWriter(OutputStreamWriter(os, "UTF-8"))
+        try {
+            // UTF-8 with BOM to fix borked UTF-8 chars in MS Excel (for nickname output)
+            // https://en.wikipedia.org/wiki/Byte_order_mark#UTF-8
+            val os = FileOutputStream("export.csv")
+            os.write(239);
+            os.write(187);
+            os.write(191);
+            val writer = PrintWriter(OutputStreamWriter(os, "UTF-8"))
 
-        // Output player information
-        writer.println("Name,${ctx.profile.username}")
-        writer.println("Team,${ctx.profile.team}")
-        writer.println("Pokecoin,${ctx.profile.currencies.get(PlayerProfile.Currency.POKECOIN)}")
-        writer.println("Stardust,${ctx.profile.currencies.get(PlayerProfile.Currency.STARDUST)}")
-        writer.println("Level,${ctx.profile.stats.level}")
-        writer.println("Experience,${ctx.profile.stats.experience}")
-        writer.println("Pokebank,${ctx.api.inventories.pokebank.pokemons.size + ctx.api.inventories.hatchery.eggs.size}/${ctx.profile.pokemonStorage}")
-        writer.println("Inventory,${ctx.api.inventories.itemBag.size()}/${ctx.profile.itemStorage}")
-        writer.println()
+            // Output player information
+            writer.println("Name,${ctx.profile.username}")
+            writer.println("Team,${ctx.profile.team}")
+            writer.println("Pokecoin,${ctx.profile.currencies.get(PlayerProfile.Currency.POKECOIN)}")
+            writer.println("Stardust,${ctx.profile.currencies.get(PlayerProfile.Currency.STARDUST)}")
+            writer.println("Level,${ctx.profile.stats.level}")
+            writer.println("Experience,${ctx.profile.stats.experience}")
+            writer.println("Pokebank,${ctx.api.inventories.pokebank.pokemons.size + ctx.api.inventories.hatchery.eggs.size}/${ctx.profile.pokemonStorage}")
+            writer.println("Inventory,${ctx.api.inventories.itemBag.size()}/${ctx.profile.itemStorage}")
+            writer.println()
 
-        // Output Pokebank
-        writer.println("Pokebank overview")
-        writer.println("ID,Name,Nickname,Favorite ?,CP,IV [%],Stamina (HP),Max Stamina (HP),Move 1,Move 2,iStamina,iAttack,iDefense,cpMultiplier,Height [m],Weight [kg],Candy,Candies to evolve")
+            // Output Pokebank
+            writer.println("Pokebank overview")
+            writer.println("ID,Name,Nickname,Favorite ?,CP,IV [%],Stamina (HP),Max Stamina (HP),Move 1,Move 2,iStamina,iAttack,iDefense,cpMultiplier,Height [m],Weight [kg],Candy,Candies to evolve")
 
-        ctx.api.inventories.pokebank.pokemons.sortedWith(compareName.thenComparing(compareIv)).map {
-            "${it.pokemonId.number},${it.pokemonId.name},\"${it.nickname}\",${it.isFavorite},${it.cp},${it.getIvPercentage()},${it.stamina},${it.maxStamina},${it.move1.name},${it.move2.name},${it.individualStamina},${it.individualAttack},${it.individualDefense},${it.cpMultiplier},${it.heightM},n/a,${it.candy},${it.candiesToEvolve}"
-        }.forEach { writer.println(it) }
+            ctx.api.inventories.pokebank.pokemons.sortedWith(compareName.thenComparing(compareIv)).map {
+                "${it.pokemonId.number},${it.pokemonId.name},\"${it.nickname}\",${it.isFavorite},${it.cp},${it.getIvPercentage()},${it.stamina},${it.maxStamina},${it.move1.name},${it.move2.name},${it.individualStamina},${it.individualAttack},${it.individualDefense},${it.cpMultiplier},${it.heightM},n/a,${it.candy},${it.candiesToEvolve}"
+            }.forEach { writer.println(it) }
 
-        Log.normal("Wrote export CSV.");
+            Log.normal("Wrote export CSV.");
 
-        writer.close()
-        os.close()
+            writer.close()
+            os.close()
+        } catch (e: Exception) {
+            Log.red("Error writing export CSV: " + e.message)
+        }
     }
 }
