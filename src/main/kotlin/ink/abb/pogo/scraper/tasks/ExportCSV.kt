32c65dc1fe8e2cd5de794f4350dd9f85a7379d45
Kevin <Kevin.Gaytant@ArgusTech.be>
2016-07-28 22:25:34 +0200

    Added "last update" field, added a bunch of new columns in the pokebank overview

--- src/main/kotlin/ink/abb/pogo/scraper/tasks/ExportCSV.kt
+++ src/main/kotlin/ink/abb/pogo/scraper/tasks/ExportCSV.kt
@@ -21,6 +21,7 @@ import ink.abb.pogo.scraper.util.pokemon.getIvPercentage
 import java.io.FileOutputStream
 import java.io.OutputStreamWriter
 import java.io.PrintWriter
+import java.text.SimpleDateFormat
 import java.util.*
 
 class ExportCSV : Task {
@@ -38,12 +39,15 @@ class ExportCSV : Task {
         }
 
         try {
+            val dateFormatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+            val dateNow = Date()
+
             // UTF-8 with BOM to fix borked UTF-8 chars in MS Excel (for nickname output)
             // https://en.wikipedia.org/wiki/Byte_order_mark#UTF-8
             val os = FileOutputStream("export.csv")
-            os.write(239);
-            os.write(187);
-            os.write(191);
+            os.write(239)
+            os.write(187)
+            os.write(191)
             val writer = PrintWriter(OutputStreamWriter(os, "UTF-8"))
 
             // Output player information
@@ -55,14 +59,18 @@ class ExportCSV : Task {
             writer.println("Experience,${ctx.profile.stats.experience}")
             writer.println("Pokebank,${ctx.api.inventories.pokebank.pokemons.size + ctx.api.inventories.hatchery.eggs.size}/${ctx.profile.pokemonStorage}")
             writer.println("Inventory,${ctx.api.inventories.itemBag.size()}/${ctx.profile.itemStorage}")
+            writer.println("Last update,${dateFormatter.format(dateNow)}")
             writer.println()
 
             // Output Pokebank
             writer.println("Pokebank overview")
-            writer.println("ID,Name,Nickname,Favorite ?,CP,IV [%],Stamina (HP),Max Stamina (HP),Move 1,Move 2,iStamina,iAttack,iDefense,cpMultiplier,Height [m],Weight [kg],Candy,Candies to evolve")
+            writer.println("Number,Name,Nickname,Favorite?,CP,IV [%],Stamina (HP),Max Stamina (HP),Move 1,Move 2,iStamina,iAttack,iDefense,cpMultiplier,Height [m],Weight [kg],Candy,Candies to evolve,Candy costs for powerup,Stardust costs for powerup,Creation Time,Base Capture Rate,Base Flee Rate,Battles Attacked,Battles Defended,Injured?,Fainted?,Level,CP after powerup,Max CP,ID")
 
             ctx.api.inventories.pokebank.pokemons.sortedWith(compareName.thenComparing(compareIv)).map {
-                "${it.pokemonId.number},${it.pokemonId.name},\"${it.nickname}\",${it.isFavorite},${it.cp},${it.getIvPercentage()},${it.stamina},${it.maxStamina},${it.move1.name},${it.move2.name},${it.individualStamina},${it.individualAttack},${it.individualDefense},${it.cpMultiplier},${it.heightM},${it.weightKg},${it.candy},${it.candiesToEvolve}"
+                val date = Date(it.creationTimeMs)
+                dateFormatter.format(date)
+
+                "${it.pokemonId.number},${it.pokemonId.name},\"${it.nickname}\",${it.isFavorite},${it.cp},${it.getIvPercentage()},${it.stamina},${it.maxStamina},${it.move1.name},${it.move2.name},${it.individualStamina},${it.individualAttack},${it.individualDefense},${it.cpMultiplier},${it.heightM},${it.weightKg},${it.candy},${it.candiesToEvolve},${it.candyCostsForPowerup},${it.stardustCostsForPowerup},${dateFormatter.format(date)},${it.baseCaptureRate},${it.baseFleeRate},${it.battlesAttacked},${it.battlesDefended},${it.isInjured},${it.isFainted},${it.level},${it.cpAfterPowerup},${it.maxCp},${it.id}"
             }.forEach { writer.println(it) }
 
             Log.normal("Wrote export CSV.")
