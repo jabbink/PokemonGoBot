f31717ddab2f207b80c8c4742c99ae87b6a3eb1f
Jasper Abbink <git@abb.ink>
2016-08-10 20:51:41 +0200

    fix getting stuck while walking and an error occurs (#961)

--- src/main/kotlin/ink/abb/pogo/scraper/Bot.kt
+++ src/main/kotlin/ink/abb/pogo/scraper/Bot.kt
@@ -141,8 +141,14 @@ class Bot(val api: PokemonGo, val settings: Settings) {
 
         runLoop(TimeUnit.SECONDS.toMillis(5), "BotLoop") {
             task(keepalive)
-            if (settings.catchPokemon)
-                task(catch)
+            if (settings.catchPokemon) {
+                try {
+                    task(catch)
+                } catch (e: Exception) {
+                    // might have errored and paused walking
+                    ctx.pauseWalking.set(false)
+                }
+            }
             if (settings.dropItems)
                 task(drop)
             if (settings.autotransfer)
