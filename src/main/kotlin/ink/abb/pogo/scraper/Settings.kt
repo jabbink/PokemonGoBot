44acd53972d88acb0d2c5ee0e22bd416d914a81c
Nashoba24 <nicolas.v1999@hotmail.fr>
2016-08-27 22:51:34 +0200

    Auto team added

--- src/main/kotlin/ink/abb/pogo/scraper/Settings.kt
+++ src/main/kotlin/ink/abb/pogo/scraper/Settings.kt
@@ -151,6 +151,8 @@ class SettingsParser(val properties: Properties) {
                 botTimeoutAfterMinutes = getPropertyIfSet("Bot times out after X minutes and waits", "bot_timeout_after_minutes", defaults.botTimeoutAfterMinutes, String::toInt),
                 botTimeoutAfterCatchingPokemon = getPropertyIfSet("Bot times out after X minutes and waits", "bot_timeout_after_catching_pokemon", defaults.botTimeoutAfterCatchingPokemon, String::toInt),
                 botTimeoutAfterVisitingPokestops = getPropertyIfSet("Bot times out after X minutes and waits", "bot_timeout_after_visiting_pokestops", defaults.botTimeoutAfterVisitingPokestops, String::toInt)
+                
+                autoTeam = getPropertyIfSet("Auto team", "auto_team", defaults.autoTeam, String::toString)
         )
     }
 
@@ -299,6 +301,8 @@ data class Settings(
         val botTimeoutAfterMinutes: Int = -1,
         val botTimeoutAfterCatchingPokemon:Int = -1,
         val botTimeoutAfterVisitingPokestops:Int = -1
+        
+        val autoTeam: String = "NEUTRAL"
 ) {
     fun withName(name: String): Settings {
         this.name = name
